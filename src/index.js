bdb5e43ea4c4feea8b1a0c84cd3afdf68941b2b4
marina <marina.shatinskaya@gmail.com>
2018-02-23 12:58:03 +0300

    solution1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  
+var arr=str.split('');
+
+var arrAs={};
+for(var i=0;i<bracketsConfig.length;i++){
+  arrAs[bracketsConfig[i][0]]=bracketsConfig[i][1];
+}
+
+for (i=0;i<arr.length-1;i++){
+  if(arrAs[arr[i]]==arr[i+1]){
+    arr.splice(i,2); // тогда удаляем 2 элемента
+     i=(i===0)?-1:i-2;
+   }
+}
+ return (arr.length == 0)?true:false;
 }
